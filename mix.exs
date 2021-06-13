9e12bd23af4c07f66f85766f9de54b427cbc9b01
shayan javani <shayan.javani@gmail.com>
2021-06-13 14:54:23 +0430

    added metadata

--- mix.exs
+++ mix.exs
@@ -7,16 +7,24 @@ defmodule Enux.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: "https://github.com/massivefermion/enux",
+      description: description(),
+      package: package()
     ]
+  end
+
+  defp description() do
+    "A module for reading variables from .env style configuration files and injecting them into your application."
   end
 
   defp package do
     [
+      name: "enux",
       licenses: ["Apache-2.0"],
       files: ["lib", "mix.exs", "README*", "LICENSE"],
       maintainers: ["Shayan Javani"],
-      links: %{"GitHub" => "https://github.com/massivefermion/Enux"}
+      links: %{"GitHub" => "https://github.com/massivefermion/enux"}
     ]
   end
 
