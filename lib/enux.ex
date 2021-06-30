f86b8d706512de9c9eeb9da9ba576e68a441486a
shayan javani <shayan.javani@gmail.com>
2021-06-30 22:05:47 +0430

    removed IO.inspects inserted for debugging

--- lib/enux.ex
+++ lib/enux.ex
@@ -36,17 +36,13 @@ defmodule Enux do
     File.stream!(path, [], :line) |> pipeline(opts)
   end
 
-  defp pipeline(content, opts) do
+  def pipeline(content, opts) do
     content
-    |> IO.inspect()
     |> Enum.filter(fn l -> String.trim(l) != "" end)
-    |> IO.inspect()
     |> Enum.map(fn l ->
       Regex.run(~r/(?<k>[A-Za-z0-9_\s]+)=(?<v>.*)/, l, capture: :all_but_first)
     end)
-    |> IO.inspect()
     |> Enum.map(fn l -> l |> Enum.map(fn i -> String.trim(i) end) end)
-    |> IO.inspect()
     |> Enum.map(fn [k, v] ->
       {k |> handle_number() |> String.to_atom(), v |> url_encode_conditional(opts)}
     end)
