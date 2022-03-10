223f8a49746884bb00d46caa9962fed9543b13ca
shayan javani <shayan.javani@gmail.com>
2022-03-10 14:23:50 +0330

    bumped jsonc version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Enux.MixProject do
   def project do
     [
       app: :enux,
-      version: "1.2.4",
+      version: "1.2.5",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -40,7 +40,7 @@ defmodule Enux.MixProject do
       {:ex_doc, "~> 0.28", only: :dev, runtime: false},
 
       # jsonc parser
-      {:jsonc, "~> 0.6", optional: true},
+      {:jsonc, "~> 0.7", optional: true},
 
       # json parsers
       {:json, "~> 1.4", optional: true},
